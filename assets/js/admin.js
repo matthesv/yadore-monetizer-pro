4a277d4af5ea22fc6477f708716b356e8e22aa97
Matthes <45894189+matthesv@users.noreply.github.com>
2025-09-22 18:23:16 +0200

    Prepare editable default templates and bump to 2.9.21

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,10 +1,10 @@
-/* Yadore Monetizer Pro v2.9.20 - Admin JavaScript (Complete) */
+/* Yadore Monetizer Pro v2.9.21 - Admin JavaScript (Complete) */
 (function($) {
     'use strict';
 
     // Global variables
     window.yadoreAdmin = {
-        version: '2.9.20',
+        version: '2.9.21',
         ajax_url: yadore_admin.ajax_url,
         nonce: yadore_admin.nonce,
         debug: yadore_admin.debug || false,
@@ -30,7 +30,7 @@
             this.initDebug();
             this.initErrorNotices();
 
-            console.log('Yadore Monetizer Pro v2.9.20 Admin - Fully Initialized');
+            console.log('Yadore Monetizer Pro v2.9.21 Admin - Fully Initialized');
         },
 
         // Dashboard functionality
