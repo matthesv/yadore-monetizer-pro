53478eb3806c4c4da03d370796ef5d1088c4f495
Matthes <45894189+matthesv@users.noreply.github.com>
2025-10-08 15:07:00 +0700

    Improve mobile admin layout spacing

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,10 +1,10 @@
-/* Yadore Monetizer Pro v3.33 - Admin JavaScript (Complete) */
+/* Yadore Monetizer Pro v3.34 - Admin JavaScript (Complete) */
 (function($) {
     'use strict';
 
     // Global variables
     window.yadoreAdmin = {
-        version: (window.yadore_admin && window.yadore_admin.version) ? window.yadore_admin.version : '3.33',
+        version: (window.yadore_admin && window.yadore_admin.version) ? window.yadore_admin.version : '3.34',
         ajax_url: yadore_admin.ajax_url,
         nonce: yadore_admin.nonce,
         debug: yadore_admin.debug || false,
